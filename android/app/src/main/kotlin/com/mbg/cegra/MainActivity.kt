b5ce9a0f8cb12f679dfa6d6c12a74653783d1354
mananga beni <manangabeni186@gmail.com>
2026-06-08 17:44:57 +0100

    initialisation du projet

---
+++ android/app/src/main/kotlin/com/mbg/cegra/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mbg.cegra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home_pafe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePafe extends StatefulWidget {
+  const HomePafe({super.key});
+
+  @override
+  State<HomePafe> createState() => _HomePafeState();
+}
+
+class _HomePafeState extends State<HomePafe> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
